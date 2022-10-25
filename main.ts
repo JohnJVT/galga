ae44ed90e4b6afc1629d670d80adfd93a7b2368a
JohnJVT <104933633+JohnJVT@users.noreply.github.com>
2022-10-24 20:24:29 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+let Spaceplane = sprites.create(img`
+    ....ffffff.........ccc..
+    ....ff22ccf.......cc4f..
+    .....ffccccfff...cc44f..
+    ....cc24442222cccc442f..
+    ...c9b4422222222cc422f..
+    ..c999b2222222222222fc..
+    .c2b99111b222222222c22c.
+    c222b111992222ccccccc22f
+    f222222222222c222ccfffff
+    .f2222222222442222f.....
+    ..ff2222222cf442222f....
+    ....ffffffffff442222c...
+    .........f2cfffc2222c...
+    .........fcc2ffffffff...
+    ..........fc2ffff.......
+    ...........fffff........
+    `, SpriteKind.Player)
+controller.moveSprite(Spaceplane, 100, 100)
+Spaceplane.setStayInScreen(true)
+info.setLife(3)
